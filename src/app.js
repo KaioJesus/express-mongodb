061e5352daae7b501dbb7a0d5fe858616b1ebd25
Kaio Jesus <kaioramos990@Hotmail.com>
2024-07-08 13:41:51 -0300

    feat: metodo post de livros

--- src/app.js
+++ src/app.js
@@ -2,9 +2,31 @@
 import express from 'express'; //do npm i express
 
 const app = express(); //executando o express;
+app.use(express.json()); // MiddleWare -> utilizado para ter acesso as requisições e respostas;
+// express.json -> executa todas as requisições que chegam como string e são convertidos para json;
 
-app.get("/", (req, res)=>{
-    res.status(200).send("Curso de NodeJs");
+const livros = [
+    {
+        id: 1,
+        título: "Divertida mente",
+    },
+    {
+        id: 2,
+        título: "Divertida mente 2",
+    }
+];
+
+app.get("/", (req, res)=>{ // dois parâmetros, a rota e requisição (req) e resposta (res)
+    res.status(200).send("Curso de Node.Js"); //recebe o status 200 e envia curso nodeJs. send -< para dados simples
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros); // enviando objetos
+})
+
+app.post("/livros", (req, res) =>{
+    livros.push(req.body); // objeto requisição, passando o corpo da requisição, nesse caso, o corpo é o id e o título, mas o id não é preciso ser enviado;
+    res.status(201).send("Livro cadastrado com sucesso!") // 201 -> registro criado
 })
 
 export default app;
